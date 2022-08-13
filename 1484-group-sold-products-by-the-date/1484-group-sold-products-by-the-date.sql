102fd006b6b59b36b1b5d5b810c8141c9e195956
yunamom <hellokin10@gmail.com>
2022-08-13 14:56:25 +0900

    Time: 665 ms (76.88%), Space: 0B (100.00%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -1,7 +1,10 @@
-# Write your MySQL query statement below
-SELECT sell_date,
-		COUNT(DISTINCT(product)) AS num_sold, 
-		GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR ',') AS products
-FROM Activities
-GROUP BY sell_date
-ORDER BY sell_date ASC
+/* Write your PL/SQL query statement below */
+select to_char(sell_date,'YYYY-MM-DD') as sell_date
+    ,count(product) as num_sold
+    ,listagg(product,',') within group (order by product) as products
+from (
+    select distinct sell_date,product
+    from activities
+    )
+group by sell_date
+order by 1;
