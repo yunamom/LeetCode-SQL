b47576ea9922aa9f58f8eea39f6b684f3f0b49a8
yunamom <hellokin10@gmail.com>
2022-08-13 13:26:48 +0900

    Time: 825 ms (41.89%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,7 +1,5 @@
-# Write your MySQL query statement below
-SELECT 
-    user_id,
-    CONCAT(UCASE(LEFT(name, 1)),LCASE(SUBSTRING(name, 2))) 
-    AS name 
-FROM users 
+/* Write your PL/SQL query statement below */
+SELECT user_id,
+    INITCAP(name) name
+FROM users
 ORDER BY user_id;
